e1822574d6464b5047fb8a26529eb741bfe2a258
CS230271 <144984411+CS230271@users.noreply.github.com>
2023-10-25 10:53:29 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,183 @@
+//
+//  main.cpp
+//  les4.cpp
+//
+//  Created by Alisher Babadjanov on 11/10/23.
+//
+
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+
+//int main (){
+//    for ( int x = 1; x < 11; x++ ) {
+//
+//        cout<< x << endl;
+//
+//
+//    }
+//
+//
+//
+//    return 0;
+//}
+//int main (){
+//    int number = 1;
+//    int holder = 0;
+//
+//    while (number <=10){
+//        cout <<number << " ";
+//        holder = holder + number;
+//        number ++;
+//
+//    }
+//    cout <<endl;
+//
+//    cout <<holder <<endl;
+//    cout <<endl;
+//}
+
+//int main(){
+//
+//    for (int x = 1; x<100; x++){
+//
+//        if (x%5 != 0) {
+//
+//            cout << x << endl;
+//
+//        }
+//    }
+//}
+
+//int main(int argc, const char * argv[]) {
+//
+//
+//    int number;
+//    cin >> number;
+//
+//    bool isPrime = true;
+//
+//    for (int i =2; i <= number/2; i ++) {
+//        isPrime = false;
+//        cout << "number " <<number << "is not prime, becausr it is divisable by " << i << endl;
+//        break;
+//
+//    }
+//
+//
+//
+//if (isPrime) {
+//    cout << "number "<< number << "is prime number" << endl;
+//
+//
+//   return 0;
+//  }
+//}
+
+
+
+
+//int main (){
+//    int amount;
+//
+//    cout<<"Enter for an amount: \n";
+//    cin>>amount;
+//
+//    for (int i=0; i<amount; i++) {
+//        for (int j=0; j<amount; j++) {
+//            cout<<"*";
+//
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+
+//int main () {
+//    int number;
+//    cout <<"Enter number " <<endl;
+//    cin >> number;
+//    int temp = number;
+//    int sum = 0;
+//    while (temp != 0) {
+//        sum = sum + temp % 10;
+//        temp = temp/10;
+//    }
+//
+//    cout << "sum of it: " << sum <<endl;
+//
+//
+//    return 0;
+//}
+
+//void Function (int number1, int number2) {
+//    int sum = number1 + number2;
+//
+//    cout <<sum << endl;
+//}
+//
+//    int main() {
+//        cout<< "first number:" <<endl;
+//        cout<< "second number: "<<endl;
+//
+//    Function ( "int number1; int number2" );
+//
+//    return 0;
+//}
+//
+
+//bool isEven (int number) {
+//    if (number %2 == 0) {
+//        return true;
+//} else {
+//    return false;
+//   }
+//}
+//
+//int main() {
+//    int number;
+//    cout<<"Enter a number:";
+//    cin>> number;
+//
+//    bool result = isEven(number);
+//
+//    if (result) {
+//        cout<< "The number is even "<<endl;
+//    } else {
+//        cout<< "The number is odd "<<endl;
+//    }
+//    return 0;
+//}
+
+//
+//int Factorial(int number) {
+//    int factorial = 1;
+//
+//    for (int i = 1; i<= number; i++) {
+//        factorial *= i;
+//    }
+//    return factorial;
+//}
+//
+//int main(){
+//    int number;
+//    cout<< " Enter a number:";
+//    cin>> number;
+//    int result = Factorial(number);
+//    cout << " The factorial of "<< number << " is: "<< result <<endl;
+//    return 0;
+//}
+
+
+
+
